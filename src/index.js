f933e31c4063a3a7fd5793edc2556b304baa4522
Ivan Rashrahovich <rashragovichivan@gmail.com>
2024-04-16 23:38:21 +0300

    create morse-decoder application

--- src/index.js
+++ src/index.js
@@ -37,8 +37,32 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
+
+console.log(expr.length);
+
 function decode(expr) {
-    // write your solution here
+    let result = ''
+
+    for (let i = 0; i < expr.length; i += 10) {
+
+    let letter = '';
+    let exprUnit = expr.slice(i, i + 10);
+
+    if (exprUnit == '**********') {
+        result += ' ';
+    } else {
+        for (let k = 0; k < exprUnit.length; k +=2) {
+
+        if (exprUnit.slice(k, k + 2) == '10') letter += '.';
+        if (exprUnit.slice(k, k + 2) == '11') letter += '-';
+
+        }
+        result += MORSE_TABLE[letter];
+    }
+    }
+
+    return result;
 }
 
 module.exports = {
